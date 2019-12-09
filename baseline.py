11baec504db70832acc94160b3d5f3b334823af2
Shivam Garg <shivgarg@live.com>
2019-12-08 18:58:40 -0600

    adding baseline

--- baseline.py
+++ baseline.py
@@ -1,169 +1,92 @@
-from model import CommandScorer
-from collections import defaultdict
-import re
+import os
+from glob import glob
+
+import gym
+import textworld.gym
+from neural_agent_baseline import NeuralAgent
 import numpy as np
-from textworld import EnvInfos
-from typing import List, Mapping, Any, Optional
-import torch
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-class NeuralAgent:
-    """ Simple Neural Agent for playing TextWorld games. """
-    MAX_VOCAB_SIZE = 1000
-    UPDATE_FREQUENCY = 10
-    LOG_FREQUENCY = 1000
-    GAMMA = 0.9
-    EPS = 0.3
+def play(agent, path, max_step=100, nb_episodes=10, verbose=True):
+    infos_to_request = agent.infos_to_request
+    infos_to_request.max_score = True  # Needed to normalize the scores.
     
-    def __init__(self) -> None:
-        self._initialized = False
-        self._epsiode_has_started = False
-        self.id2word = ["<PAD>", "<UNK>"]
-        self.word2id = {w: i for i, w in enumerate(self.id2word)}
+    gamefiles = [path]
+    if os.path.isdir(path):
+        gamefiles = glob(os.path.join(path, "*.ulx"))
         
-        self.model = CommandScorer(input_size=self.MAX_VOCAB_SIZE, hidden_size=128)
-        self.optimizer = optim.Adam(self.model.parameters(), 0.00003)
-        self.model_copy = CommandScorer(input_size=self.MAX_VOCAB_SIZE, hidden_size=128)
-        self.mode = "test"
-        self.model_copy.load_state_dict(self.model.state_dict())
-    
-    def train(self):
-        self.mode = "train"
-        self.stats = {"max": defaultdict(list), "mean": defaultdict(list)}
-        self.transitions = []
-        self.model.reset_hidden(1)
-        self.last_score = 0
-        self.no_train_step = 0
-    
-    def test(self):
-        self.mode = "test"
-        self.model.reset_hidden(1)
-        
-    @property
-    def infos_to_request(self) -> EnvInfos:
-        return EnvInfos(description=True, inventory=True, admissible_commands=True,
-                        won=True, lost=True)
-    
-    def _get_word_id(self, word):
-        if word not in self.word2id:
-            if len(self.word2id) >= self.MAX_VOCAB_SIZE:
-                return self.word2id["<UNK>"]
-            
-            self.id2word.append(word)
-            self.word2id[word] = len(self.word2id)
-            
-        return self.word2id[word]
-            
-    def _tokenize(self, text):
-        # Simple tokenizer: strip out all non-alphabetic characters.
-        text = re.sub("[^a-zA-Z0-9\- ]", " ", text)
-        word_ids = list(map(self._get_word_id, text.split()))
-        return word_ids
-
-    def _process(self, texts):
-        texts = list(map(self._tokenize, texts))
-        max_len = max(len(l) for l in texts)
-        padded = np.ones((len(texts), max_len)) * self.word2id["<PAD>"]
-
-        for i, text in enumerate(texts):
-            padded[i, :len(text)] = text
-
-        padded_tensor = torch.from_numpy(padded).type(torch.long).to(device)
-        padded_tensor = padded_tensor.permute(1, 0) # Batch x Seq => Seq x Batch
-        return padded_tensor
-      
-    def _discount_rewards(self, last_values):
-        returns, advantages = [], []
-        R = last_values.data
-        for t in reversed(range(len(self.transitions))):
-            rewards, _, _, values = self.transitions[t]
-            R = rewards + self.GAMMA * R
-            adv = R - values
-            returns.append(R)
-            advantages.append(adv)
-            
-        return returns[::-1], advantages[::-1]
-
-    def act(self, obs: str, score: int, done: bool, infos: Mapping[str, Any]) -> Optional[str]:
-        
-        # Build agent's observation: feedback + look + inventory.
-        input_ = "{}\n{}\n{}".format(obs, infos["description"], infos["inventory"])
-        
-        # Tokenize and pad the input and the commands to chose from.
-        input_tensor = self._process([input_])
-        commands_tensor = self._process(infos["admissible_commands"])
-        
-        # Get our next action and value prediction.
-        outputs, indexes, values = self.model(input_tensor, commands_tensor)
-        action = infos["admissible_commands"][indexes[0]]
-        
-        if self.mode == "test":
-            if done:
-                self.model.reset_hidden(1)
-            return action, indexes[0]
-        
-        self.no_train_step += 1
-        
-        if self.transitions:
-            reward = score - self.last_score  # Reward is the gain/loss in score.
-            self.last_score = score
-            if 'won' in infos:
-                reward += 100
-            if 'lost' in infos:
-                reward -= 100
-                
-            self.transitions[-1][0] = reward  # Update reward information.
-        
-        self.stats["max"]["score"].append(score)
-        if self.no_train_step % self.UPDATE_FREQUENCY == 0:
-            # Update model
-            returns, advantages = self._discount_rewards(values)
-            
-            loss = 0
-            for transition, ret, advantage in zip(self.transitions, returns, advantages):
-                reward, indexes_, outputs_, values_ = transition
-                
-                advantage        = advantage.detach() # Block gradients flow here.
-                probs            = F.softmax(outputs_, dim=2)
-                log_probs        = torch.log(probs)
-                log_action_probs = log_probs.gather(2, indexes_)
-                policy_loss      = (-log_action_probs * advantage).sum()
-                value_loss       = (.5 * (values_ - ret) ** 2.).sum()
-                entropy     = (-probs * log_probs).sum()
-                loss += policy_loss + 0.5 * value_loss - 0.1 * entropy
-                
-                self.stats["mean"]["reward"].append(reward)
-                self.stats["mean"]["policy"].append(policy_loss.item())
-                self.stats["mean"]["value"].append(value_loss.item())
-                self.stats["mean"]["entropy"].append(entropy.item())
-                self.stats["mean"]["confidence"].append(torch.exp(log_action_probs).item())
-            
-            if self.no_train_step % self.LOG_FREQUENCY == 0:
-                msg = "{}. ".format(self.no_train_step)
-                msg += "  ".join("{}: {:.3f}".format(k, np.mean(v)) for k, v in self.stats["mean"].items())
-                msg += "  " + "  ".join("{}: {}".format(k, np.max(v)) for k, v in self.stats["max"].items())
-                msg += "  vocab: {}".format(len(self.id2word))
-                print(msg)
-                self.stats = {"max": defaultdict(list), "mean": defaultdict(list)}
-            
-            loss.backward()
-            nn.utils.clip_grad_norm_(self.model.parameters(), 40)
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-        
-            self.transitions = []
-            self.model.reset_hidden(1)
+    env_id = textworld.gym.register_games(gamefiles,
+                                          request_infos=infos_to_request,
+                                          max_episode_steps=max_step)
+    env = gym.make(env_id)  # Create a Gym environment to play the text game.
+    if verbose:
+        if os.path.isdir(path):
+            print(os.path.dirname(path), end="")
         else:
-            # Keep information about transitions for Truncated Backpropagation Through Time.
-            self.transitions.append([None, indexes, outputs, values])  # Reward will be set on the next call
+            print(os.path.basename(path), end="")
         
-        if done:
-            self.last_score = 0  # Will be starting a new episode. Reset the last score.
+    # Collect some statistics: nb_steps, final reward.
+    avg_moves, avg_scores, avg_norm_scores = [], [], []
+    avg_score = list()
+    for no_episode in range(nb_episodes):
+        obs, infos = env.reset()  # Start new episode.
+
+        score = 0
+        done = False
+        nb_moves = 0
+        max_score = 0
+        while not done:
+            command = agent.act(obs, score, done, infos)
+            obs, score, done, infos = env.step(command)
+            max_score = max(max_score, score)
+            nb_moves += 1
+        avg_score.append(max_score)
+        agent.act(obs, score, done, infos)  # Let the agent know the game is done.
+        print('ep:{}\tlen:{}\tmax_score:{}\tavg_score:{}\tlast_score:{}'.format(no_episode, nb_moves, max_score, sum(avg_score)/len(avg_score), score))
         
-        return action, indexes[0] 
+        if verbose:
+            print(".", end="")
+        avg_moves.append(nb_moves)
+        avg_scores.append(score)
+        avg_norm_scores.append(score / infos["max_score"])
+
+    env.close()
+    msg = "  \tavg. steps: {:5.1f}; avg. score: {:4.1f} / {}."
+    if verbose:
+        if os.path.isdir(path):
+            print(msg.format(np.mean(avg_moves), np.mean(avg_norm_scores), 1))
+        else:
+            print(msg.format(np.mean(avg_moves), np.mean(avg_scores), infos["max_score"]))
+
+
+agent = NeuralAgent()
+play(agent, "./games/rewardsDense_goalDetailed.ulx")
+
+
+from time import time
+agent = NeuralAgent()
+
+print("Training")
+agent.train()  # Tell the agent it should update its parameters.
+starttime = time()
+play(agent, "./games/rewardsDense_goalDetailed.ulx", nb_episodes=500, verbose=False)  # Dense rewards game.
+print("Trained in {:.2f} secs".format(time() - starttime))
+
+agent.test()
+play(agent, "./games/rewardsDense_goalDetailed.ulx")
+
+os.system("seq 1 100 | xargs -n1 -P4 tw-make tw-simple --rewards dense --goal detailed --output training_games/ --seed")
+
+from time import time
+agent = NeuralAgent()
+
+print("Training on 100 games")
+agent.train()  # Tell the agent it should update its parameters.
+starttime = time()
+play(agent, "./training_games/", nb_episodes=100 * 5, verbose=False)  # Each game will be seen 5 times.
+print("Trained in {:.2f} secs".format(time() - starttime))
+
+os.system("seq 1 20 | xargs -n1 -P4 tw-make tw-simple --rewards dense --goal detailed --test --output testing_games/ --seed")
+agent.test()
+play(agent, "./games/rewardsDense_goalDetailed.ulx")  # Averaged over 10 playthroughs.
+play(agent, "./testing_games/", nb_episodes=20 * 10)  # Averaged over 10 playthroughs for each test game.
+play(RandomAgent(), "./testing_games/", nb_episodes=20 * 10)
