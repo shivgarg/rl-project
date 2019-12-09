a802f5ca2c4fa54699931ea6430a9e23818e3b8b
Shivam Garg <shivgarg@live.com>
2019-12-08 19:15:56 -0600

    corrected baseline code

--- neural_agent_baseline.py
+++ neural_agent_baseline.py
@@ -16,60 +16,11 @@ from model_baseline import CommandScorer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-class CommandScorer(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(CommandScorer, self).__init__()
-        torch.manual_seed(42)  # For reproducibility
-        self.embedding    = nn.Embedding(input_size, hidden_size)
-        self.encoder_gru  = nn.GRU(hidden_size, hidden_size)
-        self.cmd_encoder_gru  = nn.GRU(hidden_size, hidden_size)
-        self.state_gru    = nn.GRU(hidden_size, hidden_size)
-        self.hidden_size  = hidden_size
-        self.state_hidden = torch.zeros(1, 1, hidden_size, device=device)
-        self.critic       = nn.Linear(hidden_size, 1)
-        self.att_cmd      = nn.Linear(hidden_size * 2, 1)
-
-    def forward(self, obs, commands, **kwargs):
-        input_length = obs.size(0)
-        batch_size = obs.size(1)
-        nb_cmds = commands.size(1)
-
-        embedded = self.embedding(obs)
-        encoder_output, encoder_hidden = self.encoder_gru(embedded)
-        state_output, state_hidden = self.state_gru(encoder_hidden, self.state_hidden)
-        self.state_hidden = state_hidden
-        value = self.critic(state_output)
-
-        # Attention network over the commands.
-        cmds_embedding = self.embedding.forward(commands)
-        _, cmds_encoding_last_states = self.cmd_encoder_gru.forward(cmds_embedding)  # 1 x cmds x hidden
-
-        # Same observed state for all commands.
-        cmd_selector_input = torch.stack([state_hidden] * nb_cmds, 2)  # 1 x batch x cmds x hidden
-
-        # Same command choices for the whole batch.
-        cmds_encoding_last_states = torch.stack([cmds_encoding_last_states] * batch_size, 1)  # 1 x batch x cmds x hidden
-
-        # Concatenate the observed state and command encodings.
-        cmd_selector_input = torch.cat([cmd_selector_input, cmds_encoding_last_states], dim=-1)
-
-        # Compute one score per command.
-        scores = F.relu(self.att_cmd(cmd_selector_input)).squeeze(-1)  # 1 x Batch x cmds
-
-        probs = F.softmax(scores, dim=2)  # 1 x Batch x cmds
-        index = probs[0].multinomial(num_samples=1).unsqueeze(0) # 1 x batch x indx
-        return scores, index, value
-
-    def reset_hidden(self, batch_size):
-        self.state_hidden = torch.zeros(1, batch_size, self.hidden_size, device=device)
-
-
 class NeuralAgent:
     """ Simple Neural Agent for playing TextWorld games. """
     MAX_VOCAB_SIZE = 1000
     UPDATE_FREQUENCY = 10
-    LOG_FREQUENCY = 1
+    LOG_FREQUENCY = 1000
     GAMMA = 0.9
     
     def __init__(self) -> None:
@@ -171,7 +122,7 @@ class NeuralAgent:
             self.transitions[-1][0] = reward  # Update reward information.
         
         self.stats["max"]["score"].append(score)
-        if done: #self.no_train_step % self.UPDATE_FREQUENCY == 0:
+        if self.no_train_step % self.UPDATE_FREQUENCY == 0:
             # Update model
             returns, advantages = self._discount_rewards(values)
             
@@ -196,7 +147,7 @@ class NeuralAgent:
             
             if self.no_train_step % self.LOG_FREQUENCY == 0:
                 msg = "{}. ".format(self.no_train_step)
-                msg += "  ".join("{}: {:.3f}".format(k, np.sum(v)) for k, v in self.stats["mean"].items())
+                msg += "  ".join("{}: {:.3f}".format(k, np.mean(v)) for k, v in self.stats["mean"].items())
                 msg += "  " + "  ".join("{}: {}".format(k, np.max(v)) for k, v in self.stats["max"].items())
                 msg += "  vocab: {}".format(len(self.id2word))
                 print(msg)
